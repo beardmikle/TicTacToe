b8b8b7f90040d4ea85b5e8c330ac50b41a3a8e76
beardmikle <ankmo@ya.ru>
2023-02-13 09:21:04 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by beardmikle on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
